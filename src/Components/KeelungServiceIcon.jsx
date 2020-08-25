1501824bc4849fb4cdb9a325e2e4e352e25d8c20
Eddie Hsieh <zxcx98738@gmail.com>
2020-08-25 23:36:12 +0800

    icon finish

--- src/Components/KeelungServiceIcon.jsx
+++ src/Components/KeelungServiceIcon.jsx
@@ -4,11 +4,11 @@ import "../CSS/Home.css";
 class KeelungServiceIcon extends Component {
   render() {
     const { icon_img, text, href, colnum } = this.props;
-    const a = `d-flex column align-items-center justify-content-center ${colnum}`;
+    const a = `d-flex align-items-center justify-content-center ${colnum}`;
 
     return (
       <div className={a} style={{display: 'inline'}}>
-        <button className="icon_but mx-3 my-3">
+        <button className="icon_but my-3">
           <div className="d-flex flex-column align-items-center justify-content-center">
             <a href={href}>
               <img src={`/pictures/${icon_img}`} style={{ width: "40%" }} />
